fb519a1e804573e6fbb3df66c2284d9f7fe71f11
Jaroslav Karlík <jarinkarlos@seznam.cz>
2020-06-24 16:46:28 +0200

    Setup-ověření, jestli teče proud Loop-vypiš hodnoty, jsou-li různé od nuly, po 20 s vypiš vše Excel s měřením

--- lightgate/two_lightgates_time/two_lightgates_time.ino
+++ lightgate/two_lightgates_time/two_lightgates_time.ino
@@ -1,12 +1,13 @@
 #define startGate 2
 #define endGate 3
-
-
+ 
+ 
 volatile long startTime = 0;
 volatile long endTime = 0;
 long bulletTime = 0;
 float bulletSpeed = 0;
-
+bool bang = false; 
+ 
 void setup()
 {
   Serial.begin(9600);
@@ -14,30 +15,45 @@ void setup()
   pinMode(endGate, INPUT);
   attachInterrupt(digitalPinToInterrupt(startGate), UpdateTimeStart, FALLING);
   attachInterrupt(digitalPinToInterrupt(endGate), UpdateTimeEnd, FALLING);
+  if (digitalRead(startGate) == HIGH){
+      Serial.println("startGate ready");}
+      else{Serial.println("startGate problem");}
+  if (digitalRead(endGate) == HIGH){
+      Serial.println("endGate ready");}
+      else{Serial.println("endGate problem");}  
 }
-
+ 
 void loop() {
-  delay(1000);
-  noInterrupts();
-  bulletTime = endTime - startTime;
-  //bulletSpeed = ((float)6,36)/(float)bulletTime;
-  Serial.print("Start: ");
-  Serial.print(startTime);
-  Serial.print(" End: ");
-  Serial.print(endTime);
-  Serial.print(" Time: ");
-  Serial.println(bulletTime);
-  //Serial.print(" Speed: ");
-  //Serial.print(bulletSpeed);
-  //Serial.println(" mm/ms");
-  interrupts();
+  if (startTime > 0 && endTime > 0 && bang == false){
+    noInterrupts();
+    bulletTime = endTime - startTime;
+    //bulletSpeed = ((float)6,36)/(float)bulletTime;
+    Serial.print("Start: ");
+    Serial.print(startTime);
+    Serial.print(" End: ");
+    Serial.print(endTime);
+    Serial.print(" Time: ");
+    Serial.println(bulletTime);
+    //Serial.print(" Speed: ");
+    //Serial.print(bulletSpeed);
+    //Serial.println(" mm/ms");
+    interrupts();
+    bang = true;
+  }
+  if(millis() > 20000){
+        Serial.print("Start: ");
+        Serial.print(startTime);
+        Serial.print(" End: ");
+        Serial.print(endTime);
+        Serial.print(" Time: ");
+        Serial.println(bulletTime);}
 }
-
-
+ 
+ 
 void UpdateTimeStart() {
   startTime = micros();
 }
-
+ 
 void UpdateTimeEnd() {
   endTime = micros();
 }
